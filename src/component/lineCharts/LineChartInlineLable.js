bffde6b6a42cc49c2250d1f1a6fd19187febeac0
ABP <amolbpathak@gmail.com>
2020-05-09 18:52:52 +0530

    changed data file path
    changed data file path

--- src/component/lineCharts/LineChartInlineLable.js
+++ src/component/lineCharts/LineChartInlineLable.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
-import data from "/public/apples-bananas.tsv";
+import data from "../../data/apples-bananas.tsv";
 
 class LineChartInlineLable extends Component {
   componentDidMount() {
