be6af81a065c2f24ad25bde195a0557d51ad6d5e
ABP <amolbpathak@gmail.com>
2020-05-09 20:13:30 +0530

    Testing deployment
    Testing deployment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import "./styles.css";
 import ZoomableSunburst from "./component/paiCharts/ZoomableSunburst";
-import LineChartTooltip from "./component/lineCharts/LineChartTooltip";
-import LineChartInlineLable from "./component/lineCharts/LineChartInlineLable.js";
+// import LineChartTooltip from "./component/lineCharts/LineChartTooltip";
+// import LineChartInlineLable from "./component/lineCharts/LineChartInlineLable.js";
+import VerticalBarChart from "./component/barCharts/VerticalBarChart";
 
 export default function App() {
   return (
     <div className="App">
       <section>
         <ZoomableSunburst />
-        <LineChartInlineLable />
+        {/* <LineChartInlineLable /> */}
       </section>
       <section>
-        <LineChartTooltip />
+        {/* <LineChartTooltip /> */}
+        <VerticalBarChart />
       </section>
     </div>
   );
